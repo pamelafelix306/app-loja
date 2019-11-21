b8eac26163d0ef68f58774abf01bd033a3885059
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2019-11-21 13:49:17 +0530

    create basic items overview

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyShop',
+      title: 'My e-Com Platform',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
-      home: MyHomePage(),
+      home: ProductsOverviewScreen(),
     );
   }
 }
