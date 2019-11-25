ff5110034106c646dda827e5469f22a84fb26ea0
Arya Adarsha Gautam <arya.aag.16@gmail.com>
2019-11-25 18:01:36 +0530

    add confirm dialogs and snackbars

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -39,7 +39,6 @@ class Cart with ChangeNotifier {
     String title,
   ) {
     if (_items.containsKey(productId)) {
-      // change quantity...
       _items.update(
         productId,
         (existingCartItem) => CartItem(
@@ -59,6 +58,25 @@ class Cart with ChangeNotifier {
           quantity: 1,
         ),
       );
+    }
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    } else if (_items[productId].quantity > 1) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity - 1,
+        ),
+      );
+    } else {
+      _items.remove(productId);
     }
     notifyListeners();
   }
--- lib/widgets/product_item_widget.dart
+++ lib/widgets/product_item_widget.dart
@@ -48,6 +48,18 @@ class ProductItemWidget extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Added to Cart'),
+                  action: SnackBarAction(
+                    label: 'Undo',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
